bb264d19f7ae0e50348c878b8f265e78bd0f1078
votivetech <votive.techs@gmail.com>
2013-07-02 16:07:00 +0530

    footer view
    this is the main footer view for site...

---
+++ footer.php
@@ -0,0 +1,28 @@
+  </section>
+      
+  <div class="clear"></div>
+  
+  <footer>
+    <div class="footer">
+      <div class="about">
+        <p>(c) LiveChat. Všechna práva vyhrazena.</p>
+        <p><a href="#">Superkancelář s.r.o.</a></p>
+      </div>  
+      <div class="footer_menu">
+        <ul>
+          <li>
+            <a href="<?php echo base_url(); ?>" class="active">Home</a>
+          </li>
+          <li>
+            <a href="<?php echo base_url(); ?>home/aboutus">About us</a>
+          </li>
+          <li>
+            <a href="<?php echo base_url(); ?>home/contactus">Contact us</a>
+          </li>
+        </ul>
+      </div>
+    </div>        
+  </footer>
+
+  </body>
+</html>
